7a2434873418bb95a1b5cb270d591efa6b5e216d
Myikigai73 <110479668+Myikigai73@users.noreply.github.com>
2023-05-17 14:27:47 +0200

    Update XMLtoJSON.py

--- XMLtoJSON.py
+++ XMLtoJSON.py
@@ -1,4 +1,6 @@
 
+# -*- coding: utf-8 -*-
+
 # Program to convert an xml
 # file to json file
  
@@ -26,3 +28,30 @@ with open("test.xml") as xml_file:
     with open("data.json", "w") as json_file:
         json_file.write(json_data)
         # json_file.close()
+        
+        
+import json
+from pymongo import MongoClient
+ 
+ 
+# Making Connection
+myclient = MongoClient("mongodb://localhost:27017/")
+  
+# database
+db = myclient["GFG"]
+  
+# Created or Switched to collection
+# names: GeeksForGeeks
+Collection = db["data"]
+ 
+# Loading or Opening the json file
+with open('data.json') as file:
+    file_data = json.load(file)
+     
+# Inserting the loaded data in the Collection
+# if JSON contains data more than one entry
+# insert_many is used else insert_one is used
+if isinstance(file_data, list):
+    Collection.insert_many(file_data) 
+else:
+    Collection.insert_one(file_data)
